5e32ee83420968a94ada67e6ca70ce659a1610cc
thebaptiste <jean-baptiste.veslin@meteo.fr>
2022-05-10 09:20:45 +0000

    build: fix CI

--- .github/workflows/docker_build.sh
+++ .github/workflows/docker_build.sh
@@ -3,20 +3,11 @@
 #set -eu
 set -x
 
-docker pull $TAG_BRANCH 2>/dev/null
-HASH_BEFORE=`docker images -q $TAG_BRANCH` 
-if [ "$BRANCH" != "master" ]; then
-    docker build -t $TAG_BRANCH --build-arg BRANCH=$BRANCH --build-arg CACHEBUST=$CACHEBUST .
-else
-    docker build -t $TAG_BRANCH -t $TAG_LATEST --build-arg BRANCH=$BRANCH --build-arg CACHEBUST=$CACHEBUST .
-fi
-HASH_AFTER=`docker images -q $TAG_BRANCH`
-if [ "${HASH_BEFORE}" == "${HASH_AFTER}" ]; then
-    echo '::set-output name=dispatch_bypass::true'
-else
-    echo '::set-output name=dispatch_bypass::false'
-    docker push $TAG_BRANCH
-    if [ "$BRANCH" == "master" ]; then
-        docker push $TAG_LATEST
-    fi
+docker pull metwork/portable-envtpl-buildimage:master
+HASH_BEFORE=`docker images -q metwork/portable-envtpl-buildimage:master` 
+docker build -t metwork/portable-envtpl-buildimage:master -t metwork/portable-envtpl-buildimage:latest .
+HASH_AFTER=`docker images -q metwork/portable-envtpl-buildimage:master`
+if [ "${HASH_BEFORE}" != "${HASH_AFTER}" ]; then
+    docker push metwork/portable-envtpl-buildimage:master
+    docker push metwork/portable-envtpl-buildimage:latest
 fi
